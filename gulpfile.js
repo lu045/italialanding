30bb7c9dcc3a2ea988a48c7081b5dd05c8b2a9d8
Luis Fernando <serraluisfernando@gmail.com>
2018-12-03 08:19:02 -0200

    inicio da base da landingpage

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
-var autoprefixer = require('gulp-autoprefixer');
+
 
 //Task para o sass
 gulp.task('sass', function () {
@@ -15,18 +15,10 @@ gulp.task('watch', function () {
     gulp.watch('sass/**/*.sass', ['sass']);
 });
 
-//Task para o autoprefixer
-gulp.task('autoprefixer', () =>
-    gulp.src('css/**/*.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('css'))
-);
+
 
 //Task Default
-gulp.task('default', ['sass', 'watch', 'autoprefixer']);
+gulp.task('default', ['sass', 'watch']);
 
 
 
